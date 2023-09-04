24d069843de49f2ac9ceb096c7df065e68f73e8b
13dnizinski <danielnizinski@gmail.com>
2023-09-03 20:00:21 -0500

    Fixed data issue

--- main.py
+++ main.py
@@ -4,12 +4,9 @@ from model.user import User
 
 app = FastAPI()
 
-
-
-@app.get("/shiftspot/users/{user_id}")
+@app.get("/fast-api/users/{user_id}")
 async def get_user_id(user_id):
     dummy_user_data = User()
-    #return {"message": "Retrieved data for user "+str(user_id)}
     return dummy_user_data
 
 @app.get("/")
