bfd2435d1e92deca8520c660bf6253c4865f5774
13dnizinski <danielnizinski@gmail.com>
2023-09-03 19:56:53 -0500

    Removed comments from startup file

--- startup.sh
+++ startup.sh
@@ -11,9 +11,6 @@ cd /etc/nginx/sites-enabled/
 
 cp ../../../fastapi_nginx ./
 
-#Note that before this line runs, you must replace the "server_name" field with your EC2's public IPV4 IP Address:
-#sudo nano fastapi_nginx
-
 sudo service nginx restart
 cd
 cd fast-api
